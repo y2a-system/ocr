c7607f411c8b80a3bead0885b28289eda8f0937e
y2a-system <yeyintex@gmail.com>
2026-03-13 15:47:47 +0800

    tesseract-core-simd.wasm.js

--- tesseract-assets/tesseract-core-simd.wasm.js
+++ tesseract-assets/tesseract-core-simd.wasm.js
@@ -1,7 +1,2 @@
 Download the core files from:  
-https://github.com/naptha/tesseract.js-coreWeb Browsers
-
-Required files:
-
-tesseract-core.wasm.js
-tesseract-core-simd.wasm.js
+https://github.com/naptha/tesseract.js-core
